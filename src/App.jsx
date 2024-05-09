fb8655100f3728d44b0c96c28626e77d5310dba9
imranhwebdev <alimrancv870@gmail.com>
2024-05-10 05:04:26 +0600

    roadmap done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Launch from './components/Launch';
 import SiteMarquee from './components/SiteMarquee';
+import RoadMap from './components/RoadMap';
 // import Header from './components/Header';
 // import Header from './components/Header';
 // import Header from './components/Header';
@@ -15,6 +16,7 @@ export default function App() {
       <Banner />
       <Launch />
       <SiteMarquee />
+      <RoadMap />
       <Footer />
     </>
   )
--- src/components/RoadMap.jsx
+++ src/components/RoadMap.jsx
@@ -1,7 +1,52 @@
-import {Container, Row, Col} from 'react-bootstrap'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import { Container, Row, Col } from 'react-bootstrap'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+// import howtocloudtop from '../assets/img/howtocloudtop.png';
 
 export default function RoadMap() {
+    useEffect(() => {
+        AOS.init({
+          duration: 1000,
+        });
+      }, []);
   return (
-    <div>RoadMap</div>
+    <section className='roadmap-area'>
+        <Container>
+            <Row>
+                <Col>
+                    <div className="section-title text-center">
+                        
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+              <VerticalTimeline>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work">                  
+                    <span className='number'>Q1 2021</span>
+                    <p>Release of $COSMO meme marketplacePreparation period for $COSMO wedsite revomp dapps </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement  className="vertical-timeline-element--work">                    
+                    <span className='number'>Q2 2022</span>
+                    <p>Release of $COSMO meme marketplacePreparation period for $COSMO wedsite revomp dapps </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work">                    
+                    <span className='number'>Q3 2023</span>
+                    <p>Release of $COSMO meme marketplacePreparation period for $COSMO wedsite revomp dapps </p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work">                    
+                    <span className='number'>Q4 2024</span>
+                    <p>Release of $COSMO meme marketplacePreparation period for $COSMO wedsite revomp dapps </p>
+                </VerticalTimelineElement>
+
+              </VerticalTimeline>
+            </Row>
+        </Container>
+    </section>
   )
 }
